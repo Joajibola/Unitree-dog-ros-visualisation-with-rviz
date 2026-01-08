90c142726b372e7643a7f03388dfc661a60e00f9
Joajibola <Joajibola323@gmail.com>
2026-01-08 21:59:16 +0100

    Save local work before pulling README

--- build/dogg_description/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
+++ build/dogg_description/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
@@ -55,3 +55,6 @@ endforeach()
 message(STATUS "Execute custom uninstall script")
 
 # begin of custom uninstall code
+
+# uninstall files installed using the symlink install functions
+include("/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_symlink_install/ament_cmake_symlink_install_uninstall_script.cmake")
--- build/dogg_description/cmake_install.cmake
+++ build/dogg_description/cmake_install.cmake
@@ -43,69 +43,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description/" TYPE DIRECTORY FILES
-    "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/dogg_description/meshes"
-    "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/dogg_description/urdf"
-    )
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/dogg_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/dogg_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description/environment" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_environment_hooks/ament_prefix_path.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description/environment" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_environment_hooks/path.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_environment_hooks/local_setup.bash")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_environment_hooks/local_setup.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_environment_hooks/local_setup.zsh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_environment_hooks/local_setup.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_environment_hooks/package.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_index/share/ament_index/resource_index/packages/dogg_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description/cmake" TYPE FILE FILES
-    "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_core/dogg_descriptionConfig.cmake"
-    "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_core/dogg_descriptionConfig-version.cmake"
-    )
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dogg_description" TYPE FILE FILES "/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/dogg_description/package.xml")
+  include("/home/jaja/Task_4/Unitree_go_dog_visualisation/unitree_dog_ws/build/dogg_description/ament_cmake_symlink_install/ament_cmake_symlink_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
